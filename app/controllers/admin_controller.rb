fe810608742e3bc834b31ecb9099829832d539a9
mishrabhavesh <mishra.bhavesh@outlook.com>
2020-04-03 01:57:36 +0530

    admin page access ssolved

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 class AdminController < ApplicationController
-  access except: [:show, :edit, :create, :update, :new, :destroy, :index], user: {except:[:show, :edit, :create, :update, :new, :destroy, :index]}, site_admin: :all
-  def index
+  access except: [ :admin], user: {except:[ :admin]}, site_admin: :all
+
+  def admin
     
   end
 end
