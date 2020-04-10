13c679da8dc1a95cd88bf37244db1c2f4bea76b9
mishrabhavesh <mishra.bhavesh@outlook.com>
2020-04-11 00:51:36 +0530

    migration error solved

--- db/migrate/20200403150722_rename__seller_no.rb
+++
@@ -1,6 +0,0 @@
-class RenameSellerNo < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :tokens, :seller_no, :token_no
-    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
-  end
-end
--- db/migrate/20200404151342_remove_token_no_column.rb
+++ db/migrate/20200404151342_remove_token_no_column.rb
@@ -1,6 +1,5 @@
 class RemoveTokenNoColumn < ActiveRecord::Migration[6.0]
   def change
-    remove_column :tokens, :token_no
     add_column :tokens, :seller_id, :integer
     add_column :sellers, :token_id, :integer 
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
